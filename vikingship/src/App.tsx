c2423e720189a1403ad937c1f60c4e82cb06eda5
yilujun <yilujun@sefonsoft.com>
2020-10-22 23:49:41 +0800

    add button component

--- vikingship/src/App.tsx
+++ vikingship/src/App.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Hello world</h1>
-          <h2>Hello world</h2>
-          <h3>Hello world</h3>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+				<Button onClick={(e) => {e.preventDefault(); alert(123);}}>Hello</Button>
+				<Button disabled>Disabled Button</Button>
+				<Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+				<Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Hello</Button>
+				<Button btnType={ButtonType.Link} href="http://baidu.com">Baidu Link</Button>
+				<Button btnType={ButtonType.Link} href="http://baidu.com" disabled>Baidu Link</Button>
+                <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ vikingship/src/components/Button/Button.tsx
@@ -0,0 +1,73 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import classNames from 'classnames'
+
+export enum ButtonType {
+    Primary = 'primary',
+    Default = 'default',
+    Danger = 'danger',
+    Link = 'link'
+}
+
+export enum ButtonSize {
+    Large = 'lg',
+    Small = 'sm'
+}
+
+interface BaseButtonProps {
+    className?: string;
+    disabled?: boolean;
+    size?: ButtonSize;
+    btnType?: ButtonType;
+    children: React.ReactNode;
+    href?: string;
+}
+
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const Button: FC<ButtonProps> = props => {
+    const {
+        btnType,
+        className,
+        disabled,
+        size,
+        children,
+        href,
+        ...restProps
+    } = props
+    // btn, btn-lg, btn-primary
+    const classes = classNames('btn', className, {
+        [`btn-${btnType}`]: btnType,
+        [`btn-${size}`]: size,
+        'disabled': (btnType === 'link') && disabled
+    })
+    if (btnType === 'link' && href) {
+        return (
+            <a
+                className={classes}
+                href={href}
+                {...restProps}
+            >
+                {children}
+            </a>
+        )
+    } else {
+        return (
+            <button
+                className={classes}
+                disabled={disabled}
+                {...restProps}
+            >
+                {children}
+            </button>
+        )
+    }
+}
+
+Button.defaultProps = {
+    disabled: false,
+    btnType: ButtonType.Default
+}
+
+export default Button
