d0ff8f469f64039e23c21dec99db1502810d3a47
yilujun100 <yilujun100@qq.com>
2021-05-10 22:51:43 +0800

    feat: add reactivity API use01

---
+++ vue3-basic/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <h1>{{count}}</h1>
+        <h1>{{double}}</h1>
+        <button @click="increase">+1</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() { // setup方法是在data、props、computed、methods以及声明周期函数之前运行
+    const count = ref(0) // ref 返回一个响应式对象
+    const double = computed(() => count.value * 2)
+    const increase = () => {
+        count.value++
+    }
+
+    return {
+        count,
+        double,
+        increase
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
