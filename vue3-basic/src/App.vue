5f5875939806d557c4081292750af3b7239ea8e0
yilujun100 <yilujun100@qq.com>
2021-05-10 23:05:31 +0800

    feat: add lifecycle

--- vue3-basic/src/App.vue
+++ vue3-basic/src/App.vue
@@ -3,16 +3,22 @@
         <img alt="Vue logo" src="./assets/logo.png">
         <h1>{{count}}</h1>
         <h1>{{double}}</h1>
+        <ul>
+            <li v-for="number in numbers" :key="number"><h1>{{number}}</h1></li>
+        </ul>
+        <h1>{{person.name}}</h1>
         <button @click="increase">+1</button>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed, reactive, toRefs } from 'vue'
+import { defineComponent, ref, computed, reactive, toRefs, onMounted, onUnmounted, onUpdated, onRenderTriggered } from 'vue'
 interface DataProps {
     count: number;
     double: number;
     increase: () => void;
+    numbers: number[];
+    person: { name?: string };
 }
 
 export default defineComponent({
@@ -23,11 +29,24 @@ export default defineComponent({
     // const increase = () => {
     //     count.value++
     // }
+    onMounted(() => {
+        console.log('mounted')
+    })
+    onUpdated(() => {
+        console.log('updated')
+    })
+    onRenderTriggered(event => {
+        console.log(event)
+    })
     const data: DataProps = reactive({
         count: 0,
         increase: () => { data.count++ },
-        double: computed(() => data.count * 2)
+        double: computed(() => data.count * 2),
+        numbers: [0, 1, 2],
+        person: {}
     })
+    data.numbers[0] = 5
+    data.person.name = 'yilujun100'
     const refData = toRefs(data)
 
     return {
