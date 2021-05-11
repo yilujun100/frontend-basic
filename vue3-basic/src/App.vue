ce03f2cb71be8f38c6f44d15550d07c58b7ee35f
yilujun100 <yilujun100@qq.com>
2021-05-11 23:27:19 +0800

    feat: add generic transform

--- vue3-basic/src/App.vue
+++ vue3-basic/src/App.vue
@@ -10,7 +10,10 @@
         <h1>{{greetings}}</h1>
         <h1>X: {{x}}, Y: {{y}}</h1>
         <h1 v-if="loading">Loading...</h1>
-        <img v-if="loaded" :src="result.message" alt="">
+        <!-- dog result -->
+        <!-- <img v-if="loaded" :src="result.message" alt=""> -->
+        <!-- cat result -->
+        <img v-if="loaded" :src="result[0].url" alt="">
         <button @click="increase">+1</button><br/>
         <button @click="updateGreetings">Update Title</button>
     </div>
@@ -26,6 +29,16 @@ interface DataProps {
     increase: () => void;
     // numbers: number[];
     // person: { name?: string };
+}
+interface DogResult {
+    message: string;
+    status: string;
+}
+interface CatResult {
+    id: string;
+    url: string;
+    width: number;
+    height: number;
 }
 
 export default defineComponent({
@@ -69,7 +82,14 @@ export default defineComponent({
     // 2.可以给x,y设置别名，这样就避免了命名冲突的风险
     // 3.这部分逻辑可以脱离组件存在, 实现逻辑的复用
     const { x, y } = useMousePosition()
-    const { result, loading, loaded } = useURLLoader('https://dog.ceo/api/breeds/image/random')
+    // const { result, loading, loaded } = useURLLoader<DogResult>('https://dog.ceo/api/breeds/image/random')
+    const { result, loading, loaded } = useURLLoader<CatResult[]>('https://api.thecatapi.com/v1/images/search?limit=1')
+    watch(result, () => {
+        if (result.value) {
+            // console.log('value', result.value.message)
+            console.log('value', result.value[0].url)
+        }
+    })
     const refData = toRefs(data)
 
     return {
