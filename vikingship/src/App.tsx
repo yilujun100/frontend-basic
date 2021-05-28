2b806e94e8b99f63f3634922308718c48f86cce2
yilujun100 <yilujun100@qq.com>
2021-05-28 22:27:08 +0800

    feat: add basic upload files

--- vikingship/src/App.tsx
+++ vikingship/src/App.tsx
@@ -2,33 +2,24 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App: React.FC = () => {
-    const [title, setTitle] = useState('')
-    const postData = {
-        title: 'my title',
-        body: 'hello man'
-    }
-    useEffect(() => {
-        // get 请求
-        /* axios.get('https://jsonplaceholder.typicode.com/posts/1', {
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            responseType: 'json'
-        })
-            .then(res => {
-                setTitle(res.data.title)
-            }) */
-        // post 请求
-        axios.post('https://jsonplaceholder.typicode.com/posts', postData)
-            .then(res => {
-                setTitle(res.data.title)
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files
+        if (files) {
+            const uploadedFile = files[0]
+            const formData = new FormData()
+            formData.append(uploadedFile.name, uploadedFile)
+            axios.post('https://jsonplaceholder.typicode.com/posts', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then(res => {
+                console.log('res: ', res)
             })
-    })
+        }
+    }
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>{title}</h1>
-            </header>
+        <div className="App" style={{marginTop: '100px', marginLeft: '100px'}}>
+            <input type="file" name="myFile" onChange={handleFileChange} />
         </div>
     )
 }
