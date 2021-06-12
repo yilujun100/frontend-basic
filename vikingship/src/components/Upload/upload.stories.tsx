36b25c21c1157f45be81ec79d9e16102d0d0ad68
yilujun100 <yilujun100@qq.com>
2021-06-12 23:09:39 +0800

    feat: add UploadList code02

--- vikingship/src/components/Upload/upload.stories.tsx
+++ vikingship/src/components/Upload/upload.stories.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { Upload } from './upload'
+import { Upload, UploadFile } from './upload'
+const defaultFileList: UploadFile[] = [
+    { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percent: 30 },
+    { uid: '122', size: 1234, name: 'xyz.md', status: 'success', percent: 30 },
+    { uid: '121', size: 1234, name: 'eyiha.md', status: 'error', percent: 30 }
+]
 
 const checkFileSize = (file: File) => {
     if (Math.round(file.size / 1024) > 50) {
@@ -17,8 +22,10 @@ const filePromise = (file: File) => {
 const SimpleUpload = () => {
     return (
         <Upload
-            action="https://jsonplaceholder.typicode.com/posts"
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
             onChange={action('changed')}
+            defaultFileList={defaultFileList}
+            onRemove={action('removed')}
         />
     )
 }
--- vikingship/src/components/Upload/upload.tsx
+++ vikingship/src/components/Upload/upload.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useRef, ChangeEvent, useState } from 'react'
 import axios from 'axios'
-
+import UploadList from './uploadList'
 import Button from '../Button/button'
 export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error'
 export interface UploadFile {
@@ -15,23 +15,27 @@ export interface UploadFile {
 }
 export interface UploadProps {
     action: string;
+    defaultFileList?: UploadFile[];
     beforeUpload?: (file: File) => boolean | Promise<File>;
     onProgress?: (percentage: number, file: File) => void;
     onSuccess?: (data: any, file: File) => void;
     onError?: (err: any, file: File) => void;
     onChange?: (file: File) => void;
+    onRemove?: (file: UploadFile) => void;
 }
 export const Upload: FC<UploadProps> = props => {
     const {
         action,
+        defaultFileList,
         beforeUpload,
         onProgress,
         onSuccess,
         onError,
-        onChange
+        onChange,
+        onRemove
     } = props
     const fileInput = useRef<HTMLInputElement>(null)
-    const [ fileList, setFileList ] = useState<UploadFile[]>([])
+    const [ fileList, setFileList ] = useState<UploadFile[]>(defaultFileList || [])
     const updateFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
         setFileList(prevList => {
             return prevList.map(file => {
@@ -56,6 +60,14 @@ export const Upload: FC<UploadProps> = props => {
         uploadFiles(files)
         if (fileInput.current) {
             fileInput.current.value = ''
+        }
+    }
+    const handleRemove = (file: UploadFile) => {
+        setFileList(prevList => {
+            return prevList.filter(item => item.uid !== file.uid)
+        })
+        if (onRemove) {
+            onRemove(file)
         }
     }
     const uploadFiles = (files: FileList) => {
@@ -87,7 +99,7 @@ export const Upload: FC<UploadProps> = props => {
         setFileList([_file, ...fileList])
         const formData = new FormData()
         formData.append(file.name, file)
-        axios.post('https://jsonplaceholder.typicode.com/posts', formData, {
+        axios.post(action, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             },
@@ -132,6 +144,10 @@ export const Upload: FC<UploadProps> = props => {
                 ref={fileInput}
                 onChange={handleFileChange}
                 style={{display: 'none'}}
+            />
+            <UploadList
+                fileList={fileList}
+                onRemove={handleRemove}
             />
         </div>
     )
