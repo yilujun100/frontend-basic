1031c06633d37f65e506909b7e52c3d87bf1b7c6
yilujun100 <yilujun100@qq.com>
2021-06-13 23:41:32 +0800

    feat: add Progress component

--- vikingship/src/components/Upload/uploadList.tsx
+++ vikingship/src/components/Upload/uploadList.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { UploadFile } from './upload'
 import Icon from '../Icon/icon'
+import Progress from '../Progress/progress'
 interface UploadListProps {
     fileList: UploadFile[];
     onRemove: (_file: UploadFile) => void;
@@ -29,6 +30,7 @@ export const UploadList: FC<UploadListProps> = props => {
                         <span className="file-actions">
                             <Icon icon="times" onClick={() => { onRemove(item)}}/>
                         </span>
+                        {item.status === 'uploading' && <Progress percent={item.percent || 0} />}
                     </li>
                 )
             })}
