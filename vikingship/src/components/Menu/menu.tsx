e33472f68a92f040f86641d080226521104b8de9
yilujun100 <yilujun100@qq.com>
2021-09-25 22:18:01 +0800

    feat: 发布到npm

--- vikingship/src/components/Menu/menu.tsx
+++ vikingship/src/components/Menu/menu.tsx
@@ -28,7 +28,7 @@ export const MenuContext = createContext<IMenuContext>({ index: '0' })
 /**
  * 为网站提供导航功能的菜单。支持横向纵向两种模式，支持下拉菜单。
  * ~~~js
- * import { Menu } from 'vikingship'
+ * import { Menu } from 'vikingship-ui'
  * ~~~
  */
 export const Menu: FC<MenuProps> = props => {
